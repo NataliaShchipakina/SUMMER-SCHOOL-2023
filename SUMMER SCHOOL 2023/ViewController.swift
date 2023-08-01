c70589610c26004e36f264d041862bce118b8fca
NataliaShchipakina <shchipakinanata1i@gmail.com>
2023-08-01 10:39:08 +0300

    Initial Commit

---
+++ SUMMER SCHOOL 2023/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SUMMER SCHOOL 2023
+//
+//  Created by Natalia Shchipakina on 01.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
